1bb2f73dd797568939df97fd242e1ca626586d5a
Siarhei Padlozny <barmidrol@gmail.com>
2017-05-25 15:54:31 +0300

    Populate DB with fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,56 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user =  [
+  {
+    email:                  "admin@admin",
+    password:               "12345678",
+    password_confirmation:  "12345678",
+    role:                   "admin",
+    name:                   "admin",
+  }
+]
+
+User.create!(user)
+
+Faker::Hipster.words(rand(20)).each do |name|
+  DynamicObjectType.find_or_create_by(name: name)
+end
+
+types = DynamicObjectType.all
+users = User.all
+
+types.each do |type|
+  rand(10).times do
+    name = Faker::Lorem.word.titleize
+    field_type = 'text_field'
+    required = false
+    DynamicObjectField.find_or_create_by(
+      name: name,
+      field_type: field_type,
+      required: required,
+      dynamic_object_type_id: type.id
+    )
+  end
+end
+
+100.times do
+  dynamic_object_type_id = types.sample.id
+  user_id = users.sample.id
+  name = Faker::Lorem.word.titleize
+  description = Faker::Lorem.sentence(4)
+  address = Faker::Address.city + ', ' + Faker::Address.street_address
+  latitude = rand(53.872368295977545..53.931735872727586)
+  longitude = rand(27.497406005859375.. 27.601776123046875)
+  DynamicObject.create!(
+    name: name,
+    description: description,
+    address: address,
+    latitude: latitude,
+    longitude: longitude,
+    dynamic_object_type_id: dynamic_object_type_id,
+    user_id: user_id,
+    properties: Hash[DynamicObjectType.find(1).fields.map {|field| [field.name, Faker::Lorem.word]}]
+  )
+end
